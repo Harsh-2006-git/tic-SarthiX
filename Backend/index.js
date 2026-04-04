d39a77a5b1b7da3737122fdf80e4e473eeb6de9b
Harsh Manmode <harsh.work.098@gmail.com>
2026-04-04 10:03:24 +0530

    cross origin

--- Backend/index.js
+++ Backend/index.js
@@ -40,7 +40,8 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      "https://divya-yatra-tit.vercel.app"
+      "https://divya-yatra-tit.vercel.app",
+      "https://divyayatra-tic.onrender.com"
     ], // Add your frontend URLs
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
